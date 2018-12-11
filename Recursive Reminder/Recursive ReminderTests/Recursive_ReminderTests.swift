c690ab7dfe43914437130c95c4599b879fe7bcd8
Keenan Taggart <keenanbtaggart@gmail.com>
2018-12-11 14:39:42 -0800

    Uploading existing files
    One previous commit

---
+++ Recursive Reminder/Recursive ReminderTests/Recursive_ReminderTests.swift
@@ -0,0 +1,34 @@
+//
+//  Recursive_ReminderTests.swift
+//  Recursive ReminderTests
+//
+//  Created by Keenan Taggart on 12/11/18.
+//  Copyright © 2018 Keenan Taggart. All rights reserved.
+//
+
+import XCTest
+@testable import Recursive_Reminder
+
+class Recursive_ReminderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
